4ea9877cf20069e5639cc6bcc94e11a8bb850efc
Victor <v_tord@hotmail.com>
2022-11-04 17:53:40 -0300

    Requisito 2

--- src/routes/talkerRouter.js
+++ src/routes/talkerRouter.js
@@ -6,4 +6,12 @@ const router = express.Router();
 
 router.get('/', async (_req, res) => res.status(200).json(await readerJSON()));
 
+router.get('/:id', async (req, res) => {
+    const { id } = req.params;
+    const fileContent = await readerJSON();
+    const talkerFinded = fileContent.find((talker) => talker.id === +id);
+    if (!talkerFinded) res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+    res.status(200).json(talkerFinded);
+  });
+
 module.exports = router;
